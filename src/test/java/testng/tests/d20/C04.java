9a40b7729f202376302d274ba01ba950ee151759
buusra <busrakirsac@gmail.com>
2024-01-17 10:27:54 +0300

    v3

---
+++ src/test/java/testng/tests/d20/C04.java
@@ -0,0 +1,54 @@
+package testng.tests.d20;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class C04 {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+    }
+
+
+    @Test
+    public void test01() {
+
+        //amazon sayfasina gidiniz
+        driver.get("https://amazon.com");
+
+        //arama kutusunda iphone aratiniz
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("iphone");
+        searchBox.submit();
+        Assert.assertEquals(2,3);
+
+    }
+
+    @Test(dependsOnMethods = "test01")
+    public void test02() {
+        //sonuc yazisinin iphone icerdigini test edelim
+        String sonucYazisi =driver.findElement(By.xpath("(//*[@class='sg-col-inner'])[1]")).getText();
+        Assert.assertTrue(sonucYazisi.contains("iphone"));
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.close();
+    }
+}
+
+}
