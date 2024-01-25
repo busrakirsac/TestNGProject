a4582c67b6f10abbfa2c741badf35cb44b408d7d
buusra <busrakirsac@gmail.com>
2024-01-25 16:12:49 +0300

    v2

--- src/test/java/testng/utilities/Driver.java
+++ src/test/java/testng/utilities/Driver.java
@@ -2,31 +2,50 @@ package testng.utilities;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
 public class Driver {
 
-    static WebDriver driver;
+    private static ThreadLocal<WebDriver> driverPool = new ThreadLocal<>();
 
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
+        if (driverPool.get() == null) {
+            switch (ConfigReader.getProperty("browser")) {
+                case "chrome":
+                    driverPool.set(new ChromeDriver());
+                    break;
+                case "edge":
+                    driverPool.set(new EdgeDriver());
+                    break;
+                case "safari":
+                    driverPool.set(new SafariDriver());
+                    break;
+                case "firefox":
+                    driverPool.set(new FirefoxDriver());
+                    break;
+                default:
+                    driverPool.set(new ChromeDriver());
+            }
 
-        if (driver == null) {
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+            driverPool.get().manage().window().maximize();
+            driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         }
-
-        return driver;
-
+        return driverPool.get();
     }
 
+    private Driver() {
+        // Singleton pattern
+    }
 
-    public static void closeDriver(){
-        if (driver != null) {
-            driver.close();
-            driver = null;
+    public static void closeDriver() {
+
+        if (driverPool.get() != null) {
+            driverPool.get().quit();
+            driverPool.remove();
         }
-
     }
 }
