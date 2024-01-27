f7d1251b9f15ab5921ca4664bb13da45ac202c13
buusra <busrakirsac@gmail.com>
2024-01-27 16:43:00 +0300

    v3

---
+++ src/test/java/testng/tests/d25/C02.java
@@ -0,0 +1,26 @@
+package testng.tests.d25;
+
+import org.testng.annotations.Test;
+import testng.utilities.ConfigReader;
+import testng.utilities.Driver;
+
+public class C02 {
+
+    @Test
+    public void test01() {
+        Driver.getDriver().get(ConfigReader.getProperty("googleUrl"));
+        Driver.getDriver().get(ConfigReader.getProperty("blueRentalUrl"));
+        Driver.getDriver().get(ConfigReader.getProperty("OpenSourceUrl"));
+        Driver.closeDriver();
+    }
+
+
+    @Test
+    public void test02() {
+        Driver.getDriver().get(ConfigReader.getProperty("googleUrl"));
+        Driver.getDriver().get(ConfigReader.getProperty("blueRentalUrl"));
+        Driver.getDriver().get(ConfigReader.getProperty("OpenSourceUrl"));
+        Driver.closeDriver();
+    }
+
+}
